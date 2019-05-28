be83b3841ae2f8c6f673e990426d593752261700
Edin Jašarević <edinjasar14@gmail.com>
2019-05-28 23:55:28 +0200

    javascript tips | primjer02

---
+++ 3-javascript-tips/22.05.2019/primjer02.js
@@ -0,0 +1,22 @@
+// Napisati metodu za izračun zbroja vrijednosti elemenata arraya
+// Napisati metodu koja će vratiti broj parnih i broj neparnih elemenata arraya
+// Napisati metodu koja će vratiti prosječnu vrijednost u arrayu
+
+var roundResults = [
+    10, 22, 35, 5, 8, 1, 42, 3, 25, 4, 33, 41, 2, 6, 17, 22,
+    30, 44, 37, 9,
+    18,
+    7,
+    14,
+    31,
+    23,
+    11,
+    32,
+    15,
+    20,
+    13,
+    11,
+    36,
+    21,
+    16,
+    12];
