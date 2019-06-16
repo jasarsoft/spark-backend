0c07d6204acad00653835a4aac8b7f940e76920e
Edin Jašarević <edinjasar14@gmail.com>
2019-06-16 23:08:41 +0200

    zadatak06

---
+++ 4-nodejs/14-06-2019/zadatak05/auto.js
@@ -0,0 +1,13 @@
+const auto = {
+    stanje: 'u salonu',
+
+    start: function(){
+        this.stanje = 'start'
+    },
+
+    stop: function(){
+        this.stanje = 'stop'
+    },
+}
+
+module.exports.auto = auto;
---
+++ 4-nodejs/14-06-2019/zadatak05/server.js
@@ -0,0 +1,5 @@
+const a = require('./auto.js');
+
+console.log(a.auto.stanje);
+a.auto.stop()
+console.log(a.auto.stanje);
